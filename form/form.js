f5064daf7578bdc01347cc990da4397851d5eb33
Yasmen Ahmed <130899829+yasmen310@users.noreply.github.com>
2023-08-20 03:10:57 -0700

    Add files via upload

---
+++ form/form.js
@@ -0,0 +1,10 @@
+const wrapper=document.querySelector('.wrapper');
+const registerlink=document.querySelector('.register-link');
+const loginlink=document.querySelector('.login-link');
+
+registerlink.onclick=() => {
+    wrapper.classList.add('active');
+}
+loginlink.onclick=() => {
+    wrapper.classList.remove('active');
+}
